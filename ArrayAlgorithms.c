e2f25262855d5e4e04fbde07dc3fef7d4053430d
palak-khanna <138352699+palak-khanna@users.noreply.github.com>
2024-02-29 20:04:45 +0530

    Update ArrayAlgorithms.c

--- ArrayAlgorithms.c
+++ ArrayAlgorithms.c
@@ -5,33 +5,46 @@
 - TRAVERSING
 - INSERTING AT ANY POSITION
 - DELETION AT ANY POSITION
-- SEARCHING
+- insertion and deletion at any position cannot happen in case of arrays, because arrays are contiguous or fix-sized hence the given insertiona nd deletion are made 
+- in copies of new array, these are just smaple to insert and delete at beginning and end.
 - BUBBLE SORT
 */
 
 void display(int arr1[], int size) {
+    printf("\n----DISPLAY ARRAY----\n");
     for(int i = 0; i < size; i++) {
         printf("%d ", arr1[i]);
     }
     printf("\n");
 }
 
-int creation_arr(int size) {
-    printf("Enter the size of array to create : ");
-    scanf("%d", &size);
+void delete_beg(int arr1[], int size){
+    int new_size = size-1;
+    int arr[new_size];
     
-    int arr[size];
-    for(int i = 0; i < size; i++) {
-        arr[i] = i + 3;
-    }
+    for(int i=0; i<new_size; i++){
+        arr[i]= arr1[i+1];
+    };
     
-    display(arr, size);
+    display(arr, new_size);
+}
+
+
+void delete_end(int arr1[], int size){
+    int new_size = size-1;
+    int arr[new_size];
     
-    return 0;
+    for(int i=0; i<new_size; i++){
+        arr[i]= arr1[i];
+    };
+    
+    display(arr, new_size);
 }
 
 void insert_end(int arr1[], int size)
 {
+    printf("\n----INSERTION IN THE END----\n");
+    
     int new_size = size+1;
     int arr[new_size];
     
@@ -43,15 +56,70 @@ void insert_end(int arr1[], int size)
     };
     
     arr[new_size-1] = ele_to_insert;
+    display(arr,new_size);
 }
 
-// int count(int arr1[]){
-//     return sizeof(arr1[])/sizeof(arr1[1]);
-// }
+void insert_beg(int arr1[], int size)
+{
+    printf("\n----INSERTION IN THE BEGINNING----\n");
+    
+    int new_size = size+1;
+    int arr[new_size];
+    
+    int ele_to_insert;
+    printf("Enter the element to insert in the end: ");
+    scanf("%d",&ele_to_insert);
+    
+    arr[0] = ele_to_insert;
+    
+    for(int i=1; i<size+1; i++){
+        arr[i] = arr1[i];
+    };
+    display(arr, new_size);
+}
 
-void main(){
-    int array1[] = {1,2,6,4,5};
-    int count = sizeof(array1)/sizeof(array1[1]);
+void insertion_pos(int arr1[], int size){
+    int num, data;
+    printf("enter the position to append: ");
+    scanf("%d",&num);
+    
+    printf("enter the data to append: ");
+    scanf("%d",&data);
+    printf("\n");
+    
+    int new_size = size+1;
+    int arr[new_size];
+    
+    int count=0;
+    for(int i=0; i<new_size;i++){
+        count++;
+        while(count!=num){
+            arr[i] = arr1[i];
+            printf("value array here is %d\n",arr[i]);
+            printf("value of i here is %d\n",i);
+            break;
+        };
+        if(count==num){
+            arr[i] = data;
+            printf("value array in here is %d\n",arr[i]);
+            // i++;
+            printf("value of i here is %d\n",i);
+        }
+        else{
+            arr[i+1] = arr1[i];
+            printf("value array in here is %d\n",arr[i]);
+            // i++;
+            printf("value of i here is %d\n",i);
+        }
+            
+        printf("value here is %d\n\n",arr[i]);
+    };
+    display(arr, new_size);
+}
+
+void bubble_sort(int array1[], int count){
+    printf("\n----BUBBLE SORT----\n");
+    // printf("%d\n",count);
     display(array1,count);
     for (int j=0; j<count; j++){
         for(int i=0; i<count-1; i++){
@@ -60,8 +128,30 @@ void main(){
                 array1[i]= array1[i+1];
                 array1[i+1] = temp;
             }
-            printf("Inside the loop: ");
-            display(array1,count);
         };
     };
+    printf("Array sorted: \n");
+    display(array1,count);
+}
+
+int main() {
+    int size;
+    printf("Enter the size of array to create : ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    for(int i = 0; i < size; i++) {
+        int ele;
+        printf("enter element: ");
+        scanf("%d",&ele);
+        arr[i] = ele;
+    }
+    
+    display(arr, size);
+    // bubble_sort(arr, size);
+    // insert_end(arr,size);
+    // insert_beg(arr,size);
+    insertion_pos(arr, size);
+    
+    return 0;
 }
